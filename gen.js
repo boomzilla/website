d6cc94a004e39d725c08ce9f482059269affd644
Ian Ruotsala <ian.ruotsala@gmail.com>
2016-10-05 01:34:02 -0700

    Update gen.js

--- gen.js
+++ gen.js
@@ -6,19 +6,19 @@ var JAMES = "<title>:=<zzz> : <zzz>|<zzz>\n" + \
 "<prep>:=to|from|of|in\n" + \
 "<noun>:=alternatives|foundations|representations|narratives|crafts|biodiversity|knowledge|abjection|art|time|argentina|plants|people|ecoliteracy|change|mausoleum\n" + \
 "<vbing>:=drawing|defending|writing|rewriting|interogating|looking|re-interpretting\n" + \
-"<adj>:=real|literary|imagined|cultural|evolutionary|social|forbidden|sustainable\n"
+"<adj>:=real|literary|imagined|cultural|evolutionary|social|forbidden|sustainable\n";
 
 function parse_grammar(grammar){
 	var str_array = grammar.split("\n");
 	var dictionary = {};
-	var line
+	var line;
 	for (line in str_array)
 	{
 		var line_split = line.split(":=")
 		dictionary[line_split[0]] = line_split[1].split("|");
 	}
 	
-	return dictionary
+	return dictionary;
 }
 
 function generate_sentence(grammar_dict, rule){
@@ -31,13 +31,14 @@ function generate_sentence(grammar_dict, rule){
 	var rule;
     for rule in sub_list:
             if (rule[0] == "<"):
-                to_return += generate_sentence(grammar_dict, rule)
+                to_return += generate_sentence(grammar_dict, rule);
             else:
-                to_return += rule + " "
+                to_return += rule + " ";
     
-    return to_return
+    return to_return;
 }
 
-var grammar_dict = parse_grammar(JAMES)
-var rule = "<title>"
-var to_print = generate_sentence(grammar_dict, rule))
+var grammar_dict = parse_grammar(JAMES);
+var rule = "<title>";
+var to_print = generate_sentence(grammar_dict, rule));
+document.write(to_print);
